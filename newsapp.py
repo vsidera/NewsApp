9d4645bf177522d1c19a5ad0e70c0d827a2ec188
vsidera <siderravictor@gmail.com>
2020-09-14 13:09:55 +0300

    add api key

--- newsapp.py
+++ newsapp.py
@@ -7,3 +7,5 @@ app = Flask(__name__)
 def index():
     newsapi = NewsApiClient(api_key="6466c55e78594af19e719ad4539357ed")
     topheadlines = newsapi.get_top_headlines(sources="abc-news")
+
+    articles = topheadlines['articles']
