5db06d0262d6e6b8b20e42b1252c199b6760e84a
vsidera <siderravictor@gmail.com>
2020-09-14 13:17:11 +0300

    import render template

--- newsapp.py
+++ newsapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask ,render_template
 from newsapi import NewsApiClient
 
 app = Flask(__name__)
@@ -9,3 +9,16 @@ def index():
     topheadlines = newsapi.get_top_headlines(sources="abc-news")
 
     articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+    mylist = zip(news,desc,img)
