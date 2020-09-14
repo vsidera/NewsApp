2bab5e99c0dd78a2433fe85268f24d03e3624bf9
vsidera <siderravictor@gmail.com>
2020-09-14 15:58:35 +0300

    add the link to news

--- newsapp.py
+++ newsapp.py
@@ -6,22 +6,26 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     newsapi = NewsApiClient(api_key="6466c55e78594af19e719ad4539357ed")
-    topheadlines = newsapi.get_top_headlines(sources="abc-news")
+    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
 
     articles = topheadlines['articles']
 
     desc = []
     news = []
     img = []
+    link = []
+
 
     for i in range(len(articles)):
         myarticles = articles[i]
 
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
+        link.append(myarticles['url'])
         img.append(myarticles['urlToImage'])
+        
 
-    mylist = zip(news,desc,img)
+    mylist = zip(news,desc,link,img)
 
     return render_template('index.html' , context= mylist)
 
