03e4a03d804f31db421884de52195553398f7065
vsidera <siderravictor@gmail.com>
2020-09-14 13:07:06 +0300

    create index route

--- newsapp.py
+++ newsapp.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from newsapi import NewsApiClient
 
-
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    newsapi = NewsApiClient(api_key="6466c55e78594af19e719ad4539357ed")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news")
